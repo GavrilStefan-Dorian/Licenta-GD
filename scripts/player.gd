d65d858f7c842ee7ca0f183ddde8b3aebe407322
GavrilStefan-Dorian <stefan.dorian.gavril@gmail.com>
2025-04-09 13:17:55 +0300

    Pause Menu UI

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,7 +4,21 @@ extends CharacterBody2D
 const SPEED = 400.0
 const JUMP_VELOCITY = -350.0
 
+@onready var animations = $AnimatedSprite2D
 
+func handleInput():
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED 
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+func updateAnimation():
+	if velocity.length() == 0:
+		animations.animation = "idle"
+	else:
+		if velocity.x > 0: animations.animation = "walking"
+		
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -16,10 +30,6 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED 
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-#
+	handleInput()
 	move_and_slide()
+	updateAnimation()
